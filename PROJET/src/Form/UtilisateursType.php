4406ade68a756d64f7c579e09859115e5d968fdd
Ananth <xignir@gmail.com>
2021-03-31 16:02:20 +0200

    suite

---
+++ PROJET/src/Form/UtilisateursType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateurs;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UtilisateursType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('identifiant')
+            ->add('motdepasse')
+            ->add('nom')
+            ->add('prenom')
+            ->add('anniversaire')
+            ->add('isadmin')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateurs::class,
+        ]);
+    }
+}
